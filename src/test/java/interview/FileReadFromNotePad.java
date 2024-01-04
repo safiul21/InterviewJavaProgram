cafaea1c3ac19dbf2cd5f8f2122f3f14d232e643
safiul21 <iamsafiul@gmail.com>
2024-01-04 12:00:54 +0530

    intial version interview program

---
+++ src/test/java/interview/FileReadFromNotePad.java
@@ -0,0 +1,28 @@
+package interview;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class FileReadFromNotePad {
+    public static void main(String[] args) throws IOException {
+        //Approach-1
+
+        FileReader fr= new FileReader("E:\\21-22\\SampleFileTest\\Sample.txt");
+        BufferedReader br= new BufferedReader(fr);
+        String str;
+        while (( str=br.readLine() )!=null){
+            System.out.println(str);
+
+        }
+
+        File file = new File("E:\\21-22\\SampleFileTest\\Sample.txt");
+        Scanner sc= new Scanner(file);
+        while (sc.hasNext()){
+
+        String s=    sc.nextLine();
+            System.out.println(s);
+
+        }
+
+    }
+}
